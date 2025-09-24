4d31814f067e0d5eb1486185bc689b67d5db7e7a
FranIzquierdo <70143316+FranIzquierdo@users.noreply.github.com>
2025-09-24 13:50:55 +0200

    Update 05 F scenarios.R
    Delete names

--- 01 Models/Two sex/05 F scenarios.R
+++ 01 Models/Two sex/05 F scenarios.R
@@ -3,9 +3,6 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Modified 15/01/2025 #
 #~~~~~~~~~~~~~~~~~~~~~~
-# Francisco Izquierdo #
-# Marta Cousido       #
-#~~~~~~~~~~~~~~~~~~~~~~
 
 ## Press Ctrl + Shift + O to see the document outline
 
